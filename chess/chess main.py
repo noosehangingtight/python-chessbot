2ebfdea391baee07ac4f910ec25754a353ec65e6
ruby dreams <redwaffles13@gmail.com>
2022-06-11 20:23:05 -0400

    arranging files

---
+++ chess/chess main.py
@@ -0,0 +1,6 @@
+"""
+The main driver file it will be responsible for handling the players user input and displaying the current gamestate obj
+shout some random teacher on the internet who im learning this from thanks :)
+"""
+import pygame as p
+from  import chess engine
