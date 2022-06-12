96b5b5223c67d700a5aaafd75f943f1ff11e5ff1
ruby dreams <redwaffles13@gmail.com>
2022-06-12 00:46:59 -0400

    just a test before I go to bed

--- chess/chess_main.py
+++ chess/chess_main.py
@@ -3,4 +3,10 @@ The main driver file it will be responsible for handling the players user input 
 shout some random teacher on the internet who im learning this from thanks :)
 """
 import pygame as p
-from  import chess engine
+from chess import chess_engine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8 #chess board is 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 60 #for anim
+IMAGES = {}
